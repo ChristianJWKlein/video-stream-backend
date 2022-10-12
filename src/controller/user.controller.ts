259098079ff4c841150433cb88efba7f21ae6c23
Christian JW Klein <cklein010715@gmail.com>
2022-10-11 23:08:45 -0400

    fixed controller declaration

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -18,20 +18,21 @@ import { response } from 'express';
 
 @Controller('/api/v1/user')
 export class UserController {
-    constructor (private readonly userServerice: UserService,
-        private jwtService: JwtService)
-} { }
+  constructor(
+    private readonly userServerice: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-@Post('/signup')
-    async Signup(@Res() response, @Body() user: User) {
-        const newUser = await this.userServerice.signup(user);
-        return response.status(HttpStatus.CREATED).json({
-            newUser
-        })
-    }
-    @Post('/signin')
-    async SignIn(@Res() response, @Body() user: User) {
-        const token = await this.userServerice.signin(user, this.jwtService);
-        return response.status(HttpStatus.OK).json(token)
-    }
+  @Post('/signup')
+  async Signup(@Res() response, @Body() user: User) {
+    const newUser = await this.userServerice.signup(user);
+    return response.status(HttpStatus.CREATED).json({
+      newUser,
+    });
+  }
+  @Post('/signin')
+  async SignIn(@Res() response, @Body() user: User) {
+    const token = await this.userServerice.signin(user, this.jwtService);
+    return response.status(HttpStatus.OK).json(token);
+  }
 }
